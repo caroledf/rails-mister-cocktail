8668745d187262b1590f1df39f06d5a15f017a4e
Carole du Fretay <carole@dufretay.fr>
2019-05-14 13:01:49 +0100

    Rails mister Cocktail models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :cocktail
+  resources :dose
+  resources :ingredient
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
